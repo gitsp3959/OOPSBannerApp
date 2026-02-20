9aafc0335f435b9694adce4846e15ed21fa64637
gitsp3959 <sp3959@srmist.edu.in>
2026-02-21 04:36:27 +0530

    UC3: Refactor OOPS banner using String.join() for memory efficiency

---
+++ JavaPracticeLevel1/OOPSBannerApp.java
@@ -0,0 +1,17 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join("   ", "OOOOO", "PPPPP", "SSSSS"),
+            String.join("   ", "O   O", "P    P", "S"),
+            String.join("   ", "O   O", "PPPPP", "SSSSS"),
+            String.join("   ", "O   O", "P", "     S"),
+            String.join("   ", "OOOOO", "P", "SSSSS")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
