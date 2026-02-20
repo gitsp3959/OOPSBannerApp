3b2757cb8ec465972fb7acc72e5eb8e0330730e1
gitsp3959 <sp3959@srmist.edu.in>
2026-02-21 04:21:55 +0530

    Level 1 Practice Programs - 3 Solutions

---
+++ JavaPracticeLevel1/ArjunAgeCalculator.java
@@ -0,0 +1,13 @@
+class ArjunAgeCalculator {
+
+    public static void main(String[] args) {
+
+        String name = "Arjun";
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println(name + "'s age in " + currentYear + " is " + age);
+    }
+}
